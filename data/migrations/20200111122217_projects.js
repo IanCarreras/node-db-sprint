290d639e29c403e353f31c145b6015d03c9146f0
Ian <mus5002@aol.com>
2020-01-11 12:42:29 -0500

    wrote schema for projects, resources, and tasks

--- data/migrations/20200111122217_projects.js
+++ data/migrations/20200111122217_projects.js
@@ -1,8 +1,38 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema
+        .createTable('projects', tbl => {
+            tbl.increments()
+            tbl.text('project_name', 100)
+                .unique()
+                .notNullable()
+            tbl.text('description', 200)
+            tbl.boolean('completed')
+                .notNullable()
+                .defaultTo(false)
+        })
+
+        .createTable('resource', tbl => {
+            tbl.increments()
+            tbl.text('resource_name', 100)
+                .unique()
+                .notNullable()
+            tbl.text('description', 200)
+        })
+
+        .createTable('tasks', tbl => {
+            tbl.increments()
+            tbl.text('description', 200)
+                .notNullable()
+            tbl.text('notes', 200)
+            tbl.boolean('completed')
+                .notNullable()
+                .defaultTo(false)
+        })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resource')
+    .dropTableIfExists('projects')
 };
